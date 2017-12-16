ccd421ad07426279b27ef22f62513f97ab62cde8
Alex Rey <xelarey1997@gmail.com>
2017-12-16 03:48:07 +0100

    Añadiendo nuevas funciones

--- PlayerApp/ViewModel/HomeViewModel.cs
+++ PlayerApp/ViewModel/HomeViewModel.cs
@@ -5,6 +5,7 @@ using PlayerApp.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace PlayerApp.ViewModel
         private Cancion cancionSonando;
         private AudioPlayer _audioPlayer;
         private PlaybackState _playbackState;
+        private float currentVolume;
 
         public ObservableCollection<Cancion> Canciones
         {
@@ -30,7 +32,6 @@ namespace PlayerApp.ViewModel
                 RaisePropertyChanged("Canciones");
             }
         }
-
         public Cancion CancionSeleccionada
         {
             get => cancionSeleccionada;
@@ -40,38 +41,43 @@ namespace PlayerApp.ViewModel
                 RaisePropertyChanged("CancionSeleccionada");
             }
         }
+        public float CurrentVolume
+        {
+            get => currentVolume;
+            set
+            {
+                currentVolume = value;
+                RaisePropertyChanged("CurrentVolume");
+            }
+        }
         #endregion
 
         #region Commands
         public ICommand PlayPauseMusicCommand { get; private set; }
         public ICommand NextSongCommand { get; private set; }
         public ICommand PrevSongCommand { get; private set; }
+        public ICommand VolumeControlCommand { get; private set; }
         #endregion
 
         public HomeViewModel()
         {
-            LoadCanciones();
+            SetupFiles();
+            
             _playbackState = PlaybackState.Stopped;
+            CurrentVolume = 0.5f;
 
-            PlayPauseMusicCommand = new RelayCommand(StartPlayback);
-        }
-
-        private void LoadCanciones()
-        {
-            canciones = new ObservableCollection<Cancion>();
-            canciones.Add(new Cancion { Nombre = "Soy Peor", Album = "Unknown", Genero = "Trap", ID = 1, Artista = "Bad Bunny", Ruta = @"C:\Users\alexreyros\Downloads\Bad Bunny - Soy Peor.mp3" });
-            canciones.Add(new Cancion { Nombre = "Chambea", Album = "Unknown", Genero = "Trap", ID = 2, Artista = "Bad Bunny", Ruta = @"C:\Users\alexreyros\Downloads\Chambea - Bad Bunny  Video Oficial.mp3" });
-            canciones.Add(new Cancion { Nombre = "Soy Peor", Album = "Unknown", Genero = "Trap", ID = 3, Artista = "Bad Bunny" });
-            canciones.Add(new Cancion { Nombre = "Soy Peor", Album = "Unknown", Genero = "Trap", ID = 4, Artista = "Bad Bunny" });
-            this.RaisePropertyChanged(() => this.Canciones);
+            PlayPauseMusicCommand = new RelayCommand(PlayPauseMusicMethod);
+            NextSongCommand = new RelayCommand(NextSongMethod);
+            PrevSongCommand = new RelayCommand(PrevSongMethod);
+            VolumeControlCommand = new RelayCommand(VolumeControlValueChanged);
         }
 
         #region Command Methods
-        private void StartPlayback()
+        private void PlayPauseMusicMethod()
         {
             if (cancionSeleccionada != null)
             {
-                if(_playbackState == PlaybackState.Playing)
+                if(_playbackState == PlaybackState.Playing && cancionSeleccionada == cancionSonando)
                 {
                     _audioPlayer.Pause();
                     _playbackState = PlaybackState.Paused;
@@ -89,14 +95,106 @@ namespace PlayerApp.ViewModel
                         _playbackState = PlaybackState.Paused;
                     }
                 }
-                else if (_playbackState == PlaybackState.Stopped || _playbackState == PlaybackState.Paused)
+                else if ((_playbackState == PlaybackState.Stopped || _playbackState == PlaybackState.Paused) || cancionSeleccionada != cancionSonando)
                 {
-                    _audioPlayer = new AudioPlayer(cancionSeleccionada.Ruta, 0.5f);
+                    if (_audioPlayer != null)
+                    {
+                        _audioPlayer.Dispose();
+                        _audioPlayer = null;
+                    }
+                    _audioPlayer = new AudioPlayer(cancionSeleccionada.Ruta, CurrentVolume);
                     _audioPlayer.PlaybackStopType = AudioPlayer.PlaybackStopTypes.PlaybackStoppedReachingEndOfFile;
-                    _audioPlayer.Play(0.5);
+                    _audioPlayer.Play(CurrentVolume);
                     _playbackState = PlaybackState.Playing;
                     cancionSonando = cancionSeleccionada;
                 }
+            }
+        }
+
+        private void NextSongMethod()
+        {
+            if (Canciones != null && Canciones.Count > 0)
+            {
+                int? currentIndex = Canciones.IndexOf(Canciones.Where(x => x.ID == cancionSonando.ID).FirstOrDefault());
+                if (currentIndex.HasValue)
+                {
+                    int currentListSize = Canciones.Count - 1;
+                    if (currentIndex < currentListSize)
+                    {
+                        CancionSeleccionada = Canciones[(int)(currentIndex + 1)];
+                        PlayPauseMusicMethod();
+                    }
+                    else
+                    {
+                        CancionSeleccionada = Canciones[0];
+                        PlayPauseMusicMethod();
+                    }
+                }
+            }
+        }
+
+        private void PrevSongMethod()
+        {
+            if (Canciones != null && Canciones.Count > 0)
+            {
+                int? currentIndex = Canciones.IndexOf(Canciones.Where(x => x.ID == cancionSonando.ID).FirstOrDefault());
+                if (currentIndex.HasValue)
+                {
+                    int currentListSize = Canciones.Count - 1;
+                    if (currentIndex > 0)
+                    {
+                        CancionSeleccionada = Canciones[(int)(currentIndex - 1)];
+                        PlayPauseMusicMethod();
+                    }
+                    else
+                    {
+                        CancionSeleccionada = Canciones[currentListSize];
+                        PlayPauseMusicMethod();
+                    }
+                }
+            }
+        }
+
+        private void VolumeControlValueChanged()
+        {
+            if (_audioPlayer != null)
+            {
+                _audioPlayer.SetVolume(CurrentVolume);
+            }
+        }
+        #endregion
+
+        #region Class Methods
+        private void SetupFiles()
+        {
+            string path = Directory.GetCurrentDirectory();
+            string newDirectory = string.Format("{0}\\Musica", path);
+            if(!Directory.Exists(newDirectory))
+            {
+                Directory.CreateDirectory(newDirectory);
+            }
+            LoadCanciones(newDirectory);
+        }
+
+        private void LoadCanciones(string sDirectory)
+        {
+            canciones = new ObservableCollection<Cancion>();
+            int i = 0;
+            foreach (var filename in Directory.GetFiles(sDirectory))
+            {
+                FileInfo fi = new FileInfo(filename);
+                TagLib.File tagFile = TagLib.File.Create(filename);
+                Cancion cancion = new Cancion() {
+                    ID = i,
+                    Nombre = Path.GetFileNameWithoutExtension(fi.Name),
+                    Artista = tagFile.Tag.FirstAlbumArtist,
+                    Genero = tagFile.Tag.FirstGenre,
+                    Album = tagFile.Tag.Album,
+                    Ruta = filename
+                };
+                Canciones.Add(cancion);
+                this.RaisePropertyChanged(() => this.Canciones);
+                i++;
             }
         }
         #endregion
